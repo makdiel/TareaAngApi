7778626ee26b5eaccce87ace9ee3a723c9afe6e3
makdiel <makdiel.tabora@hotmail.com>
2024-06-17 00:35:40 -0600

    ultimo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokeapiService } from '../pokeapi.service';
 import { IDetallePokemon } from '../Interfaces/IDetallePokemon';
-
 import { IPokemon } from '../Interfaces/IPokemon';
+import { NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -18,11 +18,21 @@ export class HomeComponent implements OnInit {
   conteo: number = 0
   pokemons: IPokemon[] = []
   detallesPokemon: IDetallePokemon[] = []
-  limite: number = 150
+  limite: number = 20
   
   constructor(private pokeapiService:PokeapiService, private ruta: ActivatedRoute){}
 
   ngOnInit(): void{
+
+    this.ruta.queryParams.subscribe(params => {
+      const limiteParametro: number | undefined = params['limite']
+      if(limiteParametro){
+        this.limite = params['limite']
+      }else{
+        this.limite = 150
+      }
+    })
+    
     this.pokeapiService.getPokemons(this.limite).subscribe(pokemons =>{
       this.conteo = pokemons.count
       this.pokemons = pokemons.results
